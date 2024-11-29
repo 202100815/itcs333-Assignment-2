75b54b170fe6f744c29de5a2a91743534504d993
FatemaAlsheno <202102003@stu.uob.edu.bh>
2024-11-29 15:05:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
-$url = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100"
+// retrieve data from API and display response 
 
+$url = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100"
 $response = file_get_contents($url);
 echo $response;
 print_r($response);
@@ -10,6 +11,7 @@ $data = json_decode($response, true);
 echo $response;
 print_r($response);
 
+//
 if(!$data || !isset($data["results"])){
     die('error fetching data from API')
 }
